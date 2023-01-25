20206f4259e09c408b3514e29b33c47b5c09fba9
Yevhenii Davydenko <akazhulich@gmail.com>
2023-01-26 00:05:32 +0200

    Add handler_filters.py, custom filters

---
+++ handler_filters.py
@@ -0,0 +1,45 @@
+from datetime import datetime
+from telegram import Update
+from telegram.ext.filters import UpdateFilter
+
+from config import commands
+
+
+class EnglishFilter(UpdateFilter):
+    def filter(self, update: Update) -> bool:
+        alphabet = "abcdefghijklmnopqrstuvwxyz "
+        for letter in update.message.text:
+            if letter.lower() not in alphabet:
+                return False
+        return True
+
+
+class UkraineFilter(UpdateFilter):
+    def filter(self, update: Update) -> bool:
+        alphabet = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя "
+        for letter in update.message.text:
+            if letter.lower() not in alphabet:
+                return False
+        return True
+
+
+class TimeHandler(UpdateFilter):
+    def filter(self, update: Update) -> bool:
+        try:
+            datetime.strptime(update.message.text, "%H:%M").time()
+        except ValueError:
+            return False
+        return True
+
+
+class SpeechPart(UpdateFilter):
+    def filter(self, update: Update) -> bool:
+        if update.message.text in commands[:-2]:
+            return True
+        return False
+
+
+speech_part_filter = SpeechPart()
+time_filter = TimeHandler()
+english_filter = EnglishFilter()
+ukraine_filter = UkraineFilter()
