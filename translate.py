836e6918a4151c60a9344e624f38385d57a5de6e
Yevhenii Davydenko <akazhulich@gmail.com>
2023-01-31 14:55:13 +0200

    Add save_in_db func

--- translate.py
+++ translate.py
@@ -16,24 +16,31 @@ async def google_translate(command: str, user_id: int) -> str:
     db_words = await get_all_eng_words((user_id,))
     parsed = parse(command)
     words = []
+
     while len(words) < 9 or not parsed:
         word = choice(parsed)
         if word not in db_words:
             words.append(word)
         parsed.remove(word)
-    if command == "nouns":
+    if command == "Nouns":
         words = [f"the {word}" for word in words]
-    if command == "verbs":
+    if command == "Verbs":
         words = [f"to {word}" for word in words]
+    await save_in_db(words, user_id)
+
     result = []
     for word in words:
         translation = translator.translate(word, dest="uk")
         result.append(f"{word.split(' ')[1]} - {translation.text}")
-    save_in_db = (
-        (word.split(" ")[1], translator.translate(word, dest="uk").text, user_id) for word in words
-    )
-
-    for char in save_in_db:
-        await add_new_pair(char)
 
     return "\n".join(result)
+
+
+async def save_in_db(words: list[str], user_id: int) -> None:
+    translator = Translator(service_urls=["translate.googleapis.com"])
+    save = (
+        (word.split(" ")[1], translator.translate(word, dest="uk").text, user_id)
+        for word in words
+    )
+    for pair in save:
+        await add_new_pair(pair)
